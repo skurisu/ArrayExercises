c6141f761fc596602c7b3d6af0ae8b60647faf82
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-20 17:06:26 -1000

    completes level 1 -- standard arrays

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,54 @@
  **/
 
 // Create an empty array.
-
+var empty = [];
 
 // Create an array of the colors of the rainbow.
-
+var rainbow = ["red", "orange", "yellow", "green", "blue"];
 
 // Create an array of gems.
-
+var gems = ["ruby", "topaz", "sapphire"];
 
 // Create an array of military ranks.
-
+var ranks = ["private", "sergeant", "major", "general"];
 
 // Create an array of road signs.
-
+var signs = ["speed limit", "pedestrian xing", "construction zone"];
 
 // Create an array containing components of a tree.
-
+var tree = ["leaves", "branches", "roots"];
 
 // Create an array of prime numbers less than 20.
-
+var prime = [2, 5, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
-
+var programmers = ["what", "why", "seriously", "fuck", "yay"];
 
 // Create an array of elements of the periodic table.
-
+var tableElements = ["helium", "neon", "argon", "krypton", "xenon", "radon"];
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = ["mostly pennies because I'm broke", "maybe a dime"];
 
 // Create an array of items you bring to class daily.
-
+var items = ["laptop", "pen", "paper"];
 
 // Create an array of web developer role titles.
-
+var roles = ["system admin", "designer"];
 
 // Create an array of good luck charms.
-
+var luck = ["rabbit's foot", "four leaf clover"];
 
 // Create an array of mythical animals.
-
+var mythical = ["sphinx", "phoenix", "dragon"];
 
 // Create an array of drinks.
-
+var drinks = ["water", "coke", "beer", "whiskey"];
 
 // Create an array of emoticons.
-
+var emoji = [":)", ":("];
 
 // Create an array with the letters of the word 'array'.
-
+var arrayLetters = ["a", "r", "r", "a", "y"];
 
 
